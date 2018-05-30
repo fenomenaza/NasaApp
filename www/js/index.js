0e47ccdeb9527a00c065cb42b7ca00c0894a00d0
fenomenaza <37658735+fenomenaza@users.noreply.github.com>
2018-05-30 21:33:30 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=0lA0bcCfdVy8kpT7nGf6rdILClJ8Ip2XJEtWegPv";
         
         $("#loadImage").click(function(){
             $.ajax({
